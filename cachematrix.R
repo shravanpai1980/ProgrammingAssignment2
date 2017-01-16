3b3b465eba2fdd80cf2258da93669c9e990c56d8
shravanpai1980 <shravan.pai@gmail.com>
2017-01-15 19:39:09 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Creating a matrixobject,that can take inverse. 
+## As in example provided in question, I will create a list of functions that can
+## Get and set values of matrix and get its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverse <- NULL
+	set 	  <- function(y) {
+		x 	  <<- y    ## This changes value of matrix in the global environment
+		inverse <<- NULL ## When a new matrix is input, inverse changes to NULL
+	}
+	get 		<- function() x
+	getInverse	<- function() inverse
+	setInverse	<- function(value) inverse <<- value
+	list(set = set, get = get, getInverse = getInverse, setInverse = setInverse)
 }
 
+## CacheSolve function calculates the inverse of matrix only if it is NULL
+## If it is not NULL, then it pulls it from Cache
+## If a new matrix is input then inverseis set to NULL and hence will be calculated
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+CacheSolve <- function(x,...) {
+	a <- x$getInverse()
+	if (!is.null(a)) { ## In case of prior calculated value of inverse
+		message("calculating from cache")
+		return(a)
+	}
+	data 		<- x$get() 		##Take value of new matrix
+	a		<- solve(data)	##Calculate inverse
+	x$setInverse(a)			##Change value of inverse to this new value
+	a
+}	
